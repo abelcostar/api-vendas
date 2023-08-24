8e83c5f51a4cc91c31c0d693bae08540a0fe0187
abelcostar <abel.icpm@example.com>
2023-08-24 16:21:35 -0300

    new file:   src/modules/products/routes/products.routs.ts 	modified:   src/shared/http/routes/index.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,9 @@
+import productsRouter from "@modules/products/routes/products.routs";
 import { Router, response } from "express";
 import { request } from "http";
 
 const routes = Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ messege: 'Hello Dev!'})
-})
+routes.use('/products', productsRouter)
 
 export default routes
