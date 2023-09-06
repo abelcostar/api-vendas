c9e566f17d0c0ffec6a1841637d0c0a21fe3b5e8
abelcostar <abel.icpm@example.com>
2023-09-06 16:57:42 -0300

    modified:   package.json 	modified:   src/modules/users/services/CreateUserService.ts 	modified:   yarn.lock

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,36 +1,38 @@
 //SERVIÇO DE CRIAÇÃO DE USUÁRIO
 
-import { getCustomRepository } from "typeorm"
-import AppError from "@shared/erros/AppError"
-import User from "../typeorm/entities/Users"
-import UsersRepository from "../typeorm/repositories/UserRepository"
-
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/erros/AppError';
+import User from '../typeorm/entities/Users';
+import UsersRepository from '../typeorm/repositories/UserRepository';
+import { hash } from 'bcryptjs';
 
 interface IRequest {
-  name: string
-  email: string
-  password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
 class CreateUserService {
-  public async execute({name, email, password}: IRequest): Promise<User> {
-    const usersRepository = getCustomRepository(UsersRepository)
-    const emailExists = await usersRepository.findByEmail(email)
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
 
     if (emailExists) {
-      throw new AppError('Email addres already used')
+      throw new AppError('Email addres already used');
     }
+
+    const hashedPassword = await hash(password, 8);
 
     const user = usersRepository.create({
       name,
       email,
-      password
-    })
+      password: hashedPassword,
+    });
 
-    await usersRepository.save(user)
+    await usersRepository.save(user);
 
-    return user
+    return user;
   }
 }
 
-export default CreateUserService
+export default CreateUserService;
